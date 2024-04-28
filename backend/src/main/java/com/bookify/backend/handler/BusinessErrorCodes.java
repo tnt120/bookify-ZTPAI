c411e058e42da041c310ed4edd34d25cce19ca7f
Artur Pajor <apaj04@gmail.com>
2024-04-28 13:44:30 +0200

    [refactor] changed error serializer

--- backend/src/main/java/com/bookify/backend/handler/BusinessErrorCodes.java
+++ backend/src/main/java/com/bookify/backend/handler/BusinessErrorCodes.java
@@ -1,9 +1,9 @@
 package com.bookify.backend.handler;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -12,13 +12,13 @@ import java.io.IOException;
 
 @Getter
 @AllArgsConstructor
-@JsonSerialize(using = BusinessErrorCodes.DistanceSerializer.class)
+@JsonSerialize(using = BusinessErrorCodes.ErrorSerializer.class)
 public enum BusinessErrorCodes {
 
     NO_CODE(0, "No code", HttpStatus.NOT_IMPLEMENTED),
     INCORRECT_CURRENT_PASSWORD(300, "Current password is incorrect", HttpStatus.BAD_REQUEST),
     BAD_CREDENTIALS(301, "Login and / or password is incorrect", HttpStatus.FORBIDDEN),
-    ALREADY_EXIST(302, "User already exist", HttpStatus.CONFLICT),
+    ALREADY_EXIST(302, "User with provided email already exists", HttpStatus.CONFLICT),
     ROLE_NOT_FOUND(303, "Request role not found", HttpStatus.BAD_REQUEST),
     USER_NOT_FOUND(304, "User not found", HttpStatus.NOT_FOUND),
     ;
@@ -33,25 +33,14 @@ public enum BusinessErrorCodes {
                 .build();
     }
 
-    public static class DistanceSerializer extends StdSerializer<BusinessErrorCodes> {
+public static class ErrorSerializer extends JsonSerializer<BusinessErrorCodes> {
 
-        public DistanceSerializer() {
-            super(BusinessErrorCodes.class);
-        }
-
-        public DistanceSerializer(Class t) {
-            super(t);
-        }
-
-        public void serialize(
-                BusinessErrorCodes businessErrorCodes, JsonGenerator generator, SerializerProvider provider)
-                throws IOException {
-            generator.writeStartObject();
-            generator.writeFieldName("description");
-            generator.writeString(businessErrorCodes.getDescription());
-            generator.writeFieldName("code");
-            generator.writeNumber(businessErrorCodes.getCode());
-            generator.writeEndObject();
-        }
+    @Override
+    public void serialize(BusinessErrorCodes value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartObject();
+        gen.writeStringField("message", value.getDescription());
+        gen.writeNumberField("code", value.getCode());
+        gen.writeEndObject();
     }
+}
 }
