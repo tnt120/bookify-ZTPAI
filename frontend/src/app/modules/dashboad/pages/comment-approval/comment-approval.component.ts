e01fb35fa0f5117a86df36e1e5a7bbfb2dd637b2
tnt120 <apaj04@gmail.com>
2024-05-26 20:50:16 +0200

    [feat] added confirmation dialog before approve/delete comment on approval page

--- frontend/src/app/modules/dashboad/pages/comment-approval/comment-approval.component.ts
+++ frontend/src/app/modules/dashboad/pages/comment-approval/comment-approval.component.ts
@@ -1,9 +1,12 @@
+import { ConfirimationDialogData } from './../../../../core/models/confirmation-dialog-data.model';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CommentService } from '../../../../core/services/comment/comment.service';
 import { Comment } from '../../../books/models/comment.model';
 import { CustomSnackbarService } from '../../../../core/services/custom-snackbar/custom-snackbar.service';
+import { ConfirmationDialogComponent } from '../../../../shared/components/confirmation-dialog/confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-comment-approval',
@@ -22,7 +25,8 @@ export class CommentApprovalComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private router: Router,
     private commentService: CommentService,
-    private customSnackbarService: CustomSnackbarService
+    private customSnackbarService: CustomSnackbarService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
@@ -50,6 +54,26 @@ export class CommentApprovalComponent implements OnInit, OnDestroy {
         console.error(err);
       }
     }))
+  }
+
+  openConfirmationDialog(isDelete: boolean): void {
+    const data: ConfirimationDialogData = {
+      title: isDelete ? 'Delete comment' : 'Approve comment',
+      message: isDelete ? 'Are you sure you want to delete this comment?' : 'Are you sure you want to approve this comment?',
+      confirmText: isDelete ? 'Delete' : 'Approve'
+    }
+
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, { data });
+
+    this.subscriptions.push(dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        if (isDelete) {
+          this.deleteComment();
+        } else {
+          this.approveComment();
+        }
+      }
+    }));
   }
 
   approveComment(): void {
